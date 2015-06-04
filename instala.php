a9dd944720fc45404db5d6ef66b926b3059de92a
fredy <faramirez46@misena.edu.co>
2015-06-04 11:32:23 -0500

    Insert a la base de datos. Tabla Favorito

---
+++ instala.php
@@ -0,0 +1,37 @@
+<?php
+
+//INSERT A TODAS LAS TABLAS
+
+//INSERT TABLA FAVORITOS
+// $conexion = mysql_connect("localhost","fredy","fredy");
+// if (!$conexion) {
+// 	die("No se ha podido realizar la conexion:".mysql_error());
+
+// }
+// mysql_select_db("pagfavoritos",$conexion);
+// // mysql_query("INSERT INTO favorito (Usuario, Contrasena, Titulo, Direccion, Categoria, Comentario, Valoracion) VALUES ('Fredy','12345','Ninja Web','http://www.ninjawebcorporation.com/','Tecnologia','Cursos Virtuales',10)");
+// mysql_query("INSERT INTO favorito (Usuario, Contrasena, Titulo, Direccion, Categoria, Comentario, Valoracion) VALUES ('Juan','567891','YouTube','https://www.youtube.com/?gl=CO&hl=es-419','Tecnologia','Busqueda de Videos',10)");
+// mysql_close($conexion);
+
+//INSERT TABLA USUARIO
+// $conexion = mysql_connect("localhost","fredy","fredy");
+// if (!$conexion) {
+// 	die("No se ha podido realizar la conexion:".mysql_error());
+// }
+// mysql_select_db("pagfavoritos",$conexion);
+// mysql_query("INSERT INTO usuario (Usuario, Contrasena, Nombre, Apellido, Edad, Permisos) VALUES('Fredy','12345','Fredy','Ramirez',18,1)");
+// mysql_query("INSERT INTO suario (Usuario, Contrasena, Nombre, Apellido, Edad, Permisos) VALUES('Juan','567891','Juan','Acevedo',20,2)");
+
+// mysql_close($conexion);
+
+//INSERT TABLA LOGS
+// $conexion = mysql_connect("localhost","fredy","fredy");
+// if (!$conexion) {
+// 	die("No se ha podido realizar la conexion:".mysql_error());
+// }
+// mysql_select_db("pagfavoritos",$conexion);
+// mysql_query("INSERT INTO logs (Utc, Anio, Mes, Dia, Hora, Minuto, Segundo, Ip, Navegador,Usuario, Contrasena) VALUES(00000000,2015,06,02,09,49,00,127-0-0-1,'chrome','fredy','12345')");
+// mysql_query("INSERT INTO logs (Utc, Anio, Mes, Dia, Hora, Minuto, Segundo, Ip, Navegador,Usuario, Contrasena) VALUES(11111111,2015,07,03,09,49,00,127-18-0-1,'chrome','Juan','567891')");
+
+// mysql_close($conexion);
+?>
